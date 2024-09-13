35471d34abd72437c35610d13282b309e667f8fc
korey <kmmacgill@gmail.com>
2024-09-13 13:40:49 -0700

    initial commit - just getting started on project.

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Story of Korey</h1>
+      <div className="page-body">Welcome!</div>
+    </div>
+  );
+}
+
+export default App;
